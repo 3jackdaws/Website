e5013c783e63921812be468711cc49b6ea4c92b0
Ian Murphy <3jackdaws@gmail.com>
2016-08-20 01:12:22 -0700

    added SecurityAgent class.

---
+++ assets/php/SecurityAgent.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ian
+ * Date: 8/20/16
+ * Time: 12:22 AM
+ */
+set_include_path(realpath($_SERVER['DOCUMENT_ROOT']) . '/assets/php');
+require_once 'Database.php';
+
+class SecurityAgent
+{
+    private $sensitivity = 20;
+    public function __construct($sensitivity)
+    {
+        if($sensitivity) $this->sensitivity = $sensitivity;
+        $this->checkFrequency();
+    }
+
+    public function LogAction($action){
+        $action;
+        $now = time();
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $sql = "INSERT INTO ip_log (ip, action, time) VALUES(:ip, :action, FROM_UNIXTIME(:now));";
+        $stmt = Database::connect()->prepare($sql);
+        $stmt->bindParam(":ip", $ip);
+        $stmt->bindParam(":action", $action);
+        $stmt->bindParam(":now", $now);
+        $stmt->execute();
+    }
+
+    public function checkFrequency(){
+        $timemin1 = time() - 60;
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $sql = "SELECT COUNT(ip) FROM ip_log WHERE ip=:ip AND time > FROM_UNIXTIME(:timemin1);";
+        $stmt = Database::connect()->prepare($sql);
+        $stmt->bindParam(":ip", $ip);
+        $stmt->bindParam(":timemin1", $timemin1);
+        $stmt->execute();
+        $freq = $stmt->fetch()[0];
+        if($freq > $this->sensitivity) trigger_error(self::DISCONNECT_SPAM, E_USER_ERROR);
+        else{
+            sleep($freq - 10);
+        }
+    }
+
+    const DISCONNECT_SPAM = "Too many requests in a given time.";
+
+}
