caf48c86b56fd609e52267f1f37b0395a137d53b
Ian Murphy <3jackdaws@gmail.com>
2016-09-05 18:40:08 -0700

    Added test running webpage

---
+++ assets/php/runtests.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ian Murphy
+ * Date: 9/5/2016
+ * Time: 1:08 PM
+ */
+
+?>
+
+<html>
+<head>
+    <title>Algorithms Club</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/custom.css">
+    <link rel="stylesheet" href="/assets/css/search_user.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+</head>
+<body style="background-color: #eee">
+
+<div class="container">
+    <div class="col-lg-12">
+        <div class="card">
+            <center>
+                <h1 style="font-size: 3.5em; font-weight: 100">AA Testing</h1>
+            </center>
+
+        </div>
+
+    </div>
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="list-group">
+                <a class="list-group-item list-group-item-heading"><b>Run all tests</b></a>
+            <?php
+            $files = glob("./tests/*");
+            foreach ($files as $testfile){
+                echo "<a href='#' onclick='runTest(\"". basename($testfile) . "\", this)' class='list-group-item'>" . basename($testfile) . "</a>";
+            }
+            ?>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div id="console" class="card" style="color: #CCC; background-color: #333; font-family: monospace; height: 70vh; overflow-y: scroll;">
+        </div>
+    </div>
+</div>
+</body>
+<script>
+    function runTest(testScript, element){
+        var base = "/assets/php/tests/";
+        var div = element;
+        get(base + testScript, "", function (data, responsetype) {
+            var newstuff = "";
+            newstuff += "<span style=\"color: #fff\">" + testScript + "</span> &middot; ";
+            if(responsetype == 500){
+                div.className += " list-group-item-danger";
+                newstuff += "<span style=\"color: #FF5555\">Test Failed</span><br>";
+            }
+            else if(responsetype == 200){
+                div.className += " list-group-item-success";
+                newstuff += "<span style=\"color: #55FF55\">Test Succeeded</span><br>";
+            }
+
+            newstuff+= data + "<hr style='border-color: #444'>";
+
+            document.getElementById("console").innerHTML = newstuff + document.getElementById("console").innerHTML
+        })
+    }
+
+    function get(resource, parameters, callback){
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState == 4){
+                callback(xhr.responseText, xhr.status);
+            }
+        };
+        xhr.open("GET", resource + parameters, true);
+        xhr.send();
+    }
+</script>
--- assets/php/tests/accounttest.php
+++ assets/php/tests/accounttest.php
@@ -5,35 +5,24 @@
  * Date: 8/19/2016
  * Time: 9:39 PM
  */
-
-require_once '../Account.php';
-require_once "../SecurityAgent.php";
-
-$action = get("action");
-//$user = get("user");
-//$password = get("password");
-//$token = get("token");
-//$email = get("email");
+require_once realpath($_SERVER['DOCUMENT_ROOT']) . "/assets/php/StdHeader.php";
+require_once "Account.php";
 $user = "testuser";
 $password = "testpassword";
 $email = "tommy@tommydrum.net";
 
-$agent = new SecurityAgent();
-$agent->LogAction("ACCOUNT:" . $action);
 
 $account = new Account($user, $password);
 if($account == null)
-    printf("Register failed.");
+    echo "Register failed.";
 else
 {
     $account->createNew($user, $email, $password);
-    printf("Registered account, new token is %s", $account->getNewToken());
-    printf("Username is %s", $account->getUsername());
-    printf("Email is %s", $account->getEmail());
-    printf("GetToken returns %s", $account->getToken());
-    printf("Password Hash is %s", $account->getPasswordHash());
-    printf("Removing account from database");
+    echo "Registered account, new token is ". $account->getNewToken();
+    echo "Username is ". $account->getUsername();
+    echo "Email is ". $account->getEmail();
+    echo "GetToken returns ". $account->getToken();
+    echo "Password Hash is ". $account->getPasswordHash();
+    echo "Removing account from database";
     $account->removeFromDatabase();
 }
-
-//Note to Ian, On line 92 of Account.php, would you please printf instead of die, so the script can still continue?
